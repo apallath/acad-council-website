f8bf11c392b609a273c4a35e6554d5ea24ae284e
Akash Pallath <akash.pallath@iitgn.ac.in>
2017-12-27 22:00:34 +0530

    Added categories (toggle) to Experiences page

--- experiences.php
+++ experiences.php
@@ -68,70 +68,233 @@
 
       <div class="row">
 
-        <!-- Blog Entries Column -->
-        <div class="col-md-8">
-
-          <h1 class="my-4">Opinions</h1>
-
-          <h2>Internship Experiences</h2>
-
-          <cms:pages masterpage='post.php' folder='intern-experience' paginate='1' limit='2'>
-          <!-- Blog Post -->
-          <div class="card mb-4">
-            <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
-            <div class="card-body">
-              <h2 class="card-title"><cms:show k_page_title /></h2>
-              <cms:editable name='math_group' type='richtext'>
-              <p class="card-text"><cms:show post_summary/></p>
-              </cms:editable>
-              <a href="<cms:show k_page_link />" class="btn btn-outline-dark">More &rarr;</a>
-            </div>
-            <div class="card-footer text-muted">
-              <p>Posted on <cms:show k_page_date format='jS M, y'/> by <cms:show post_author/> &bull; <a href="#"><cms:show k_comments_count/> Comments</a></p>
-            </div>
-          </div>
-
-          <!-- Pagination -->
-          <cms:if k_paginated_bottom>
-          <ul class="pagination justify-content-center mb-4">
-            <cms:if k_paginate_link_prev>
-            <li class="page-item">              
-               <a class="page-link" href="<cms:show k_paginate_link_prev/>">&larr; Previous Posts</a>
-            </li>
-            <cms:else/>
-            <li class="page-item disabled">              
-               <a class="page-link" href="#">&larr; Previous Posts</a>
-            </li>
-            </cms:if>
-            
-            <cms:if k_paginate_link_next>
-            <li class="page-item">              
-               <a class="page-link" href="<cms:show k_paginate_link_next/>">More Posts &rarr;</a>
-            </li>
-            <cms:else/>
-            <li class="page-item disabled">              
-               <a class="page-link" href="#">More Posts &rarr;</a>
-            </li>
-            </cms:if>
-          </ul>
-          </cms:if>
-          </cms:pages>
-
-
-        </div>
-
         <!-- Sidebar Widgets Column -->
         <div class="col-md-4">
 
           <!-- Side Widget -->
           <div class="card my-4">
-            <h5 class="card-header">What's this about?</h5>
+            <h5 class="card-header">Experiences</h5>
             <div class="card-body">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!
             </div>
           </div>
 
+          <!-- Side Widget -->
+          <div class="card my-4">
+            <h5 class="card-header">Pick a Category</h5>
+            <div class="card-body">
+              <ul class="nav flex-column nav-pills">
+                <li class="nav-item">
+                  <a class="nav-link active" data-toggle="tab" href="#intern">Internship Experiences</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#conf">Conference/Workshop Experiences</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#comp">Competition/Contest Experiences</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#other">Other Experiences</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
         </div>
+
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+
+          <br>
+
+          <div class="tab-content">
+              <!--Internships-->
+              <div id="intern" class="tab-pane fade-in active">
+                  <cms:pages masterpage='post.php' folder='intern-experience' paginate='1' limit='2'>
+                  <h2>Internship Experiences</h2>
+                  <!-- Blog Post -->
+                  <div class="card mb-4">
+                    <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+                    <div class="card-body">
+                      <h2 class="card-title"><cms:show k_page_title /></h2>
+                      <cms:editable name='math_group' type='richtext'>
+                      <p class="card-text"><cms:show post_summary/></p>
+                      </cms:editable>
+                      <a href="<cms:show k_page_link />" class="btn btn-outline-dark">More &rarr;</a>
+                    </div>
+                    <div class="card-footer text-muted">
+                      <p>Posted on <cms:show k_page_date format='jS M, y'/> by <cms:show post_author/> &bull; <a href="#"><cms:show k_comments_count/> Comments</a></p>
+                    </div>
+                  </div>
+
+                  <!-- Pagination -->
+                  <cms:if k_paginated_bottom>
+                  <ul class="pagination justify-content-center mb-4">
+                    <cms:if k_paginate_link_prev>
+                    <li class="page-item">              
+                       <a class="page-link" href="<cms:show k_paginate_link_prev/>">&larr; Previous Posts</a>
+                    </li>
+                    <cms:else/>
+                    <li class="page-item disabled">              
+                       <a class="page-link" href="#">&larr; Previous Posts</a>
+                    </li>
+                    </cms:if>
+                    
+                    <cms:if k_paginate_link_next>
+                    <li class="page-item">              
+                       <a class="page-link" href="<cms:show k_paginate_link_next/>">More Posts &rarr;</a>
+                    </li>
+                    <cms:else/>
+                    <li class="page-item disabled">              
+                       <a class="page-link" href="#">More Posts &rarr;</a>
+                    </li>
+                    </cms:if>
+                  </ul>
+                  </cms:if>
+                  </cms:pages>
+                </div>
+
+              <!--Conferences-->
+              <div id="conf" class="tab-pane fade">
+                  <cms:pages masterpage='post.php' folder='conf-workshop-experience' paginate='1' limit='2'>
+                  <h2>Conference/Workshop Experiences</h2>
+                  <!-- Blog Post -->
+                  <div class="card mb-4">
+                    <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+                    <div class="card-body">
+                      <h2 class="card-title"><cms:show k_page_title /></h2>
+                      <cms:editable name='math_group' type='richtext'>
+                      <p class="card-text"><cms:show post_summary/></p>
+                      </cms:editable>
+                      <a href="<cms:show k_page_link />" class="btn btn-outline-dark">More &rarr;</a>
+                    </div>
+                    <div class="card-footer text-muted">
+                      <p>Posted on <cms:show k_page_date format='jS M, y'/> by <cms:show post_author/> &bull; <a href="#"><cms:show k_comments_count/> Comments</a></p>
+                    </div>
+                  </div>
+
+                  <!-- Pagination -->
+                  <cms:if k_paginated_bottom>
+                  <ul class="pagination justify-content-center mb-4">
+                    <cms:if k_paginate_link_prev>
+                    <li class="page-item">              
+                       <a class="page-link" href="<cms:show k_paginate_link_prev/>">&larr; Previous Posts</a>
+                    </li>
+                    <cms:else/>
+                    <li class="page-item disabled">              
+                       <a class="page-link" href="#">&larr; Previous Posts</a>
+                    </li>
+                    </cms:if>
+                    
+                    <cms:if k_paginate_link_next>
+                    <li class="page-item">              
+                       <a class="page-link" href="<cms:show k_paginate_link_next/>">More Posts &rarr;</a>
+                    </li>
+                    <cms:else/>
+                    <li class="page-item disabled">              
+                       <a class="page-link" href="#">More Posts &rarr;</a>
+                    </li>
+                    </cms:if>
+                  </ul>
+                  </cms:if>
+                  </cms:pages>
+                </div>
+
+              <!--Competition-->
+              <div id="comp" class="tab-pane fade">
+                  <cms:pages masterpage='post.php' folder='competition-experience' paginate='1' limit='2'>
+                  <h2>Competition/Contest Experiences</h2>
+                  <!-- Blog Post -->
+                  <div class="card mb-4">
+                    <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+                    <div class="card-body">
+                      <h2 class="card-title"><cms:show k_page_title /></h2>
+                      <cms:editable name='math_group' type='richtext'>
+                      <p class="card-text"><cms:show post_summary/></p>
+                      </cms:editable>
+                      <a href="<cms:show k_page_link />" class="btn btn-outline-dark">More &rarr;</a>
+                    </div>
+                    <div class="card-footer text-muted">
+                      <p>Posted on <cms:show k_page_date format='jS M, y'/> by <cms:show post_author/> &bull; <a href="#"><cms:show k_comments_count/> Comments</a></p>
+                    </div>
+                  </div>
+
+                  <!-- Pagination -->
+                  <cms:if k_paginated_bottom>
+                  <ul class="pagination justify-content-center mb-4">
+                    <cms:if k_paginate_link_prev>
+                    <li class="page-item">              
+                       <a class="page-link" href="<cms:show k_paginate_link_prev/>">&larr; Previous Posts</a>
+                    </li>
+                    <cms:else/>
+                    <li class="page-item disabled">              
+                       <a class="page-link" href="#">&larr; Previous Posts</a>
+                    </li>
+                    </cms:if>
+                    
+                    <cms:if k_paginate_link_next>
+                    <li class="page-item">              
+                       <a class="page-link" href="<cms:show k_paginate_link_next/>">More Posts &rarr;</a>
+                    </li>
+                    <cms:else/>
+                    <li class="page-item disabled">              
+                       <a class="page-link" href="#">More Posts &rarr;</a>
+                    </li>
+                    </cms:if>
+                  </ul>
+                  </cms:if>
+                  </cms:pages>
+                </div>
+
+              <!--Other-->
+              <div id="other" class="tab-pane fade">
+                  <cms:pages masterpage='post.php' folder='other-experience' paginate='1' limit='2'>
+                  <h2>Other Experiences</h2>
+                  <!-- Blog Post -->
+                  <div class="card mb-4">
+                    <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+                    <div class="card-body">
+                      <h2 class="card-title"><cms:show k_page_title /></h2>
+                      <cms:editable name='math_group' type='richtext'>
+                      <p class="card-text"><cms:show post_summary/></p>
+                      </cms:editable>
+                      <a href="<cms:show k_page_link />" class="btn btn-outline-dark">More &rarr;</a>
+                    </div>
+                    <div class="card-footer text-muted">
+                      <p>Posted on <cms:show k_page_date format='jS M, y'/> by <cms:show post_author/> &bull; <a href="#"><cms:show k_comments_count/> Comments</a></p>
+                    </div>
+                  </div>
+
+                  <!-- Pagination -->
+                  <cms:if k_paginated_bottom>
+                  <ul class="pagination justify-content-center mb-4">
+                    <cms:if k_paginate_link_prev>
+                    <li class="page-item">              
+                       <a class="page-link" href="<cms:show k_paginate_link_prev/>">&larr; Previous Posts</a>
+                    </li>
+                    <cms:else/>
+                    <li class="page-item disabled">              
+                       <a class="page-link" href="#">&larr; Previous Posts</a>
+                    </li>
+                    </cms:if>
+                    
+                    <cms:if k_paginate_link_next>
+                    <li class="page-item">              
+                       <a class="page-link" href="<cms:show k_paginate_link_next/>">More Posts &rarr;</a>
+                    </li>
+                    <cms:else/>
+                    <li class="page-item disabled">              
+                       <a class="page-link" href="#">More Posts &rarr;</a>
+                    </li>
+                    </cms:if>
+                  </ul>
+                  </cms:if>
+                  </cms:pages>
+                </div>
+            </div>
+        </div>
+
+        
 
       </div>
       <!-- /.row -->
